a329cc95208fc461ea06c3cc6abff2690f62f316
JJ <jjelwood2005@gmail.com>
2024-09-26 14:56:04 +0200

    Links parsing

--- db_creation.py
+++ db_creation.py
@@ -1,24 +1,29 @@
 import mysql.connector
 import wikigraph_sql_export
 
-db = mysql.connector.connect(
+hard_reset = True
+
+connection = mysql.connector.connect(
   host="localhost",
   user="root",
   password="password"
 )
 
-cursor = db.cursor()
+cursor = connection.cursor(buffered=True)
 
-cursor.execute("CREATE DATABASE wikigraph")
+try:
+    if hard_reset:
+        cursor.execute("DROP DATABASE IF EXISTS wikimap")
 
-cursor.execute("USE wikigraph")
+    cursor.execute("CREATE DATABASE IF NOT EXISTS wikimap")
 
-if cursor.execute("SHOW TABLES LIKE 'articles'") != 1:
-    cursor.execute("""CREATE TABLE `articles` (
+    cursor.execute("USE wikimap")
+
+    cursor.execute("""CREATE TABLE IF NOT EXISTS `articles` (
         `id` int NOT NULL,
-        `name` varchar(45) DEFAULT NULL,
+        `name` varchar(255) DEFAULT NULL,
         `coordinates` varchar(45) DEFAULT NULL,
-        `url` varchar(45) DEFAULT NULL,
+        `url` varchar(255) DEFAULT NULL,
         `length` int DEFAULT NULL,
         `citations` int DEFAULT NULL,
         `edits` int DEFAULT NULL,
@@ -30,26 +35,30 @@ if cursor.execute("SHOW TABLES LIKE 'articles'") != 1:
     ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
     """)
 
-if cursor.execute("SHOW TABLES LIKE 'comments'") != 1:
-    cursor.execute("""CREATE TABLE `comments` (
-        `id` int NOT NULL AUTO_INCREMENT,
+    cursor.execute("""CREATE TABLE IF NOT EXISTS `comments` (
+        `id` int AUTO_INCREMENT,
         `article_id` int DEFAULT NULL,
-        `article_name` varchar(45) DEFAULT NULL,
-        `text` varchar(45) DEFAULT NULL,
+        `article_name` varchar(255) DEFAULT NULL,
+        `text` varchar(255) DEFAULT NULL,
         `date` datetime DEFAULT NULL,
-        `username` varchar(45) DEFAULT NULL,
+        `username` varchar(255) DEFAULT NULL,
         PRIMARY KEY (`id`)
     ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
     """)
 
-if cursor.execute("SHOW TABLES LIKE 'links'") != 1:
-    cursor.execute("""CREATE TABLE `links` (
-        `id` int NOT NULL,
+    cursor.execute("""CREATE TABLE IF NOT EXISTS `links` (
+        `id` int NOT NULL AUTO_INCREMENT,
         `from_id` int DEFAULT NULL,
         `to_id` int DEFAULT NULL,
-        `from_name` varchar(45) DEFAULT NULL,
-        `to_name` varchar(45) DEFAULT NULL,
+        `from_name` varchar(255) DEFAULT NULL,
+        `to_name` varchar(255) DEFAULT NULL,
         PRIMARY KEY (`id`)
-    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+    );
     """)
 
+    wikigraph_sql_export.export_wikigraph(cursor, 'C:\\Users\\jj\\University\\uc3m\\wikimap\\sources\\wikilink_graph.2018-03-01.csv')
+
+    connection.commit()
+finally:
+    cursor.close()
+    connection.close()
--- wikigraph_sql_export.py
+++ wikigraph_sql_export.py
@@ -2,16 +2,20 @@ import csv
 import mysql.connector
 
 def export_wikigraph(cursor, path):
-    with open(path, 'r', newline='') as f:
+    with open(path, 'r', newline='', encoding="utf8") as f:
         reader = csv.DictReader(f, delimiter='\t')
         count = 0
         for row in reader:
-            count += 1
             if count >= 10000:
                 break
-            from_id = int(row['from_id'])
-            to_id = int(row['to_id'])
-            from_name = row['from_name']
-            to_name = row['to_name']
-            link_id = 
-            cursor.execute(f"INSERT INTO `links` (id, from_id, to_id, from_name, to_name) VALUES ({row[]})")
+            from_id = int(row['page_id_from'])
+            to_id = int(row['page_id_to'])
+            from_name = row['page_title_from']
+            to_name = row['page_title_to']
+            print(from_id, to_id, from_name, to_name)
+            cursor.execute(
+                "INSERT INTO links (from_id, to_id, from_name, to_name) VALUES (%s, %s, %s, %s)",
+                (from_id, to_id, from_name, to_name)
+            )
+            count += 1
+        f.close()
